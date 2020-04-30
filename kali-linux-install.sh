4ec908ca284ac35fd06e34ebc9b9d8388d5a546f
Stefan McGregor <41021963+stefanmcgregor@users.noreply.github.com>
2020-04-30 02:35:29 -0700

    Update kali-linux-install.sh

--- kali-linux-install.sh
+++ kali-linux-install.sh
@@ -1,5 +1,17 @@
-#Kali-linux-default non-interactive install script for Debian 9
+# Kali-linux-default non-interactive install script for Debian
+# Tested on Debian 9
+# Change Kali and VNC password variables below
 
+kalipass='defaultkalipassword123'
+vncpass='defaultkalipassword123'
+
+# Set Locales
+locale-gen en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales --default-priority
 
 # Ensure dependency
 apt-get -y install dirmngr
@@ -35,8 +47,8 @@ echo wireshark-common wireshark-common/install-setuid boolean false | debconf-se
 echo kismet-capture-common kismet-capture-common/install-setuid boolean true | debconf-set-selections
 echo kismet-capture-common kismet-capture-common/install-users string | debconf-set-selections
 echo sslh sslh/inetd_or_standalone select standalone | debconf-set-selections
-
 echo dictionaries-common dictionaries-common/selecting_ispell_wordlist_default note	| debconf-set-selections
+echo ucf ucf/changeprompt_threeway select keep_current | debconf-set-selections
 
 
 # Get latest information from the repos
@@ -53,13 +65,44 @@ apt-get -y install kali-linux-default        # The Default Kali Linux Install (m
 # Upgrade old packages to the latest
 apt-get -y update
 apt-get -y upgrade
-apt-get -y dist-upgrade
+
+# Dist-upgrade 
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 apt-get -y autoremove
 
-#Install and Setup XRDP
-apt-get -y install xrdp
+# Add Kali user with default password
+useradd kali -m -p $kalipass
+usermod -a -G sudo kali
+chsh -s /bin/bash kali
 
+# Install and setup X11VNC
+apt-get install x11vnc -y
+
+# Specify password to be used for VNC Connection 
+x11vnc -storepasswd $vncpass /etc/x11vnc.pass
+
+# Create the VNC service unit file
+cat > /lib/systemd/system/x11vnc.service << EOF
+[Unit]
+Description=Start x11vnc at startup.
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /etc/x11vnc.pass -rfbport 65520 -shared
+
+[Install]
+WantedBy=multi-user.target
+EOF
+ 
+systemctl enable x11vnc.service
+systemctl daemon-reload
 
 #Update the databases
 updatedb
 mandb
+
+# Sleep and shutdown
+sleep  5s
+sudo shutdown -r now 
+
